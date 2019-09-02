cfc129b6ae370c74517f1d1a4162bfb52afaf75a
Valentin Perret <valentin.perret@oto.ai>
2019-09-02 12:10:32 +0200

    Fixed install scripts

--- macos.sh
+++ macos.sh
@@ -1,9 +1,9 @@
+cp vimrc .vimrc
+cp zshrc .zshrc
+cp condarc .condarc
 cd $HOME
 mkdir .antigen
 brew install zsh
 curl -L git.io/antigen > .antigen/antigen.zsh
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-mv vimrc .vimrc
-mv zshrc .zshrc
-mv condarc .condarc
 vim +PlugInstall +qall
