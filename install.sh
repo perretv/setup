f8b6b0bea82ef1a4e702549103166f8942678904
perretv <perret.valentin@gmail.com>
2022-02-06 14:46:51 +0100

    fixed install script

--- install.sh
+++ install.sh
@@ -1,15 +1,18 @@
-#!/bin/zsh
-
 # Install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> $HOME/.profile
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> $HOME/.zprofile
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 # Install starship shell & utilities
-brew install htop wget curl zsh starship
+brew install curl git htop starship wget zsh
+
+# CPU architecture
+export CPUTYPE="$(uname -m)"
 
 # OS specific commands
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
     export OSNAME="Linux"
-    sudo apt install zsh git curl
     mkdir -p ~/.config/matplotlibrc
     cp matplotlibrc ~/.config/matplotlibrc/
 elif [[ "$OSTYPE" == "darwin"* ]]; then
@@ -18,18 +21,20 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
     mkdir ~/.matplotlib
     cp matplotlibrc ~/.matplotlib
 fi
+
 echo "OS name: ${OSNAME}"
 echo "CPU architecture: $CPUTYPE"
 
 # Install miniforge
-test -e ~/miniforge3 || {
+test -e $HOME/miniforge3 || {
     export MINIFORGE_NAME="Miniforge3-${OSNAME}-${CPUTYPE}.sh";
     wget "https://github.com/conda-forge/miniforge/releases/latest/download/${MINIFORGE_NAME}";
     sh $MINIFORGE_NAME -b; rm $MINIFORGE_NAME;
 }
+export PATH=$HOME/miniforge3/bin:$PATH
 
 # Install poetry
-curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/install-poetry.py | python -
+curl -sSL https://install.python-poetry.org | python3 - -p
 
 # Copy config files if non-existent
 test -e ~/.vimrc || cp vimrc ~/.vimrc
@@ -43,7 +48,7 @@ test -d ~/.antigen || {
 
 # Install Vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PlugInstall +qall >/dev/null 2>&1
+vim +'PluginInstall --sync' +qa
 
 # Install powerline fonts
 git clone https://github.com/powerline/fonts.git --depth=1
