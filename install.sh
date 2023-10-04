e074aabeca0ae04cfe0531d43b23659845a68747
Valentin Perret <valentin.perret@unity3d.com>
2023-10-04 12:45:31 +0000

    ipython config + updated install script

--- install.sh
+++ install.sh
@@ -3,19 +3,31 @@
 # CPU architecture
 export CPUTYPE="$(uname -m)"
 
+# Install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
 # OS specific commands
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
     export OSNAME="Linux"
     sudo apt install zsh
     mkdir -p ~/.config/matplotlibrc
     cp matplotlibrc ~/.config/matplotlibrc/
+    # Homebrew config
+    (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> $HOME/.profile
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+    # Instal GitHub CLI
+    type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
+    curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+    && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+    && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+    && sudo apt update \
+    && sudo apt install gh -y
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     export OSNAME="MacOSX"
     xcode-select --install
     mkdir ~/.matplotlib
     cp matplotlibrc ~/.matplotlib
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    brew install curl git htop wget hstr
+    brew install curl git htop wget hstr gh
 fi
 
 echo "OS name: ${OSNAME}"
@@ -37,16 +49,28 @@ test -e $HOME/miniforge3 || {
 }
 export PATH=$HOME/miniforge3/bin:$PATH
 
+# Login to GitHub
+gh auth login
+
+# Setup git
+gh auth setup-git
+git config --global user.email "valentin.perret@unity3d.com"
+git config --global user.name "Valentin Perret"
+git config --global push.default current
+
 # Install poetry
 curl -sSL https://install.python-poetry.org | python3 - -p
 
-# Copy config files if non-existent
-test -e ~/.vimrc || cp vimrc ~/.vimrc
-test -e ~/.zshrc || cp zshrc ~/.zshrc
-test -e ~/.condarc || cp condarc ~/.condarc
+# Copy config files
+cp vimrc ~/.vimrc
+cp zshrc ~/.zshrc
+cp condarc ~/.condarc
+mkdir ~/.ipython/profile_default/ && cp python_configuration.py ~/.ipython/profile_default/
 
-# Install Vundle
+# Install Vundle & plugins
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+
 
 # Install powerline fonts
 git clone https://github.com/powerline/fonts.git --depth=1
